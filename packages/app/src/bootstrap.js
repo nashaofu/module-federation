27753beb14c57b46b5bfb3d85ccfba7f3c9b5c42
nashaofu <diaocheng@outlook.com>
2021-03-07 20:44:55 +0800

    feat: 动态导入模块

--- packages/app/src/bootstrap.js
+++ packages/app/src/bootstrap.js
@@ -3,8 +3,11 @@ import add from 'app1/add'
 import subtract from 'app2/subtract'
 
 console.log(add(11, subtract(20, 10)), subtract(20, 10))
-// const { default: math } = await importRemoteModule({
-//   url: 'http://localhost:3003/microservices.js',
-//   scope: 'app3',
-//   module: './math'
-// })
+
+importRemoteModule({
+  url: 'http://localhost:3003/microservices.js',
+  scope: 'app3',
+  module: './math'
+}).then(({ default: math }) => {
+  console.log(math)
+})
---
+++ packages/app3/src/math.js
@@ -0,0 +1,8 @@
+export default {
+  abs(x) {
+    return Math.abs(x)
+  },
+  add(a, b) {
+    return a + b
+  }
+}
